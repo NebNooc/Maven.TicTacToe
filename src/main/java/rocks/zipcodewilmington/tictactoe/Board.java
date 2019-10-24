0c06981b74e380de62d193c1076ec9172a72150c
Ben Coon <bencoon@zipcoders-MacBook-Pro-22.local>
2019-10-24 17:34:08 -0400

    Finished Tic Tac Toe

--- src/main/java/rocks/zipcodewilmington/tictactoe/Board.java
+++ src/main/java/rocks/zipcodewilmington/tictactoe/Board.java
@@ -4,23 +4,60 @@ package rocks.zipcodewilmington.tictactoe;
  * @author leon on 6/22/18.
  */
 public class Board {
+    private Character[][] board;
+
     public Board(Character[][] matrix) {
+        this.board = matrix;
     }
 
+    private Boolean horizontal(Character x) {
+        for (int i = 0; i < 3; i++) {
+            if ((board[0][i] == x) && (board[1][i] == x) && (board[2][i] == x)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private Boolean vertical(Character x) {
+        for (int i = 0; i < 3; i++) {
+            if ((board[i][0] == x) && (board[i][1] == x) && (board[i][2] == x)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private Boolean diagonal(Character x) {
+        if ((board[0][0] == x) && (board[1][1] == x) && (board[2][2] == x)){
+            return true;
+        } else if ((board[2][0] == x) && (board[1][1] == x) && (board[0][2] == x)){
+            return true;
+        }
+        return false;
+    }
+
+
     public Boolean isInFavorOfX() {
-        return null;
+        return horizontal('X') || vertical('X') || diagonal('X');
     }
 
     public Boolean isInFavorOfO() {
-        return null;
+        return horizontal('O') || vertical('O') || diagonal('O');
     }
 
     public Boolean isTie() {
-        return null;
+        return !(isInFavorOfX() || isInFavorOfO());
     }
 
     public String getWinner() {
-        return null;
+        if(isInFavorOfX()){
+            return "X";
+        } else if (isInFavorOfO()){
+            return "O";
+        } else {
+            return "";
+        }
     }
 
 }
